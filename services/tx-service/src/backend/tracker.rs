daed255825e039648c39865620a306cb205f9497
Daniel <sanchez.quiros.daniel@gmail.com>
2026-06-04 11:04:24 +0200

    Use layered sorted txs

--- services/tx-service/src/backend/tracker.rs
+++ services/tx-service/src/backend/tracker.rs
@@ -191,8 +191,27 @@ where
         }
     }
 
-    pub fn get_ready_txs(&self) -> Vec<Tx> {
-        self.ready_txs.values().map(|tx| Tx::clone(tx)).collect()
+    pub fn get_ready_txs(&self) -> impl Iterator<Item = Vec<Tx>> {
+        let mut moved = self.clone();
+        std::iter::once(
+            self.ready_txs
+                .values()
+                .map(|tx| Tx::clone(tx))
+                .collect::<Vec<_>>(),
+        )
+        .chain((1..).map(move |deps_count: usize| {
+            moved
+                .tx_pending_count
+                .iter()
+                .filter_map(|(id, count)| {
+                    if *count == deps_count {
+                        pop(&mut moved.orphan_txs, id).map(|tx| Tx::clone(tx.as_ref()))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        }))
     }
 
     pub fn force_remove_tx(&mut self, id: &Tx::Hash) -> bool {

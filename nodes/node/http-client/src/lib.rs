9b5b537d1f7f81287e3adcc4918d81bfd0122c0c
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-08-18 08:38:40 -0300

    fix(http-client): accept legacy block root field

--- nodes/node/http-client/src/lib.rs
+++ nodes/node/http-client/src/lib.rs
@@ -56,6 +56,7 @@ pub struct ApiHeader {
     pub id: HeaderId,
     pub parent_block: HeaderId,
     pub slot: Slot,
+    #[serde(alias = "block_root")]
     pub body_root: ContentId,
     pub proof_of_leadership: Groth16LeaderProof,
 }
